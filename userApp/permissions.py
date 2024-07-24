68943e2ab545a2a1b5cd3d897251363214cc9787
Otabek Tusunov <otabecktursunov@gmail.com>
2024-07-24 19:18:02 +0500

    u: permissions, user serializers!;

--- userApp/permissions.py
+++ userApp/permissions.py
@@ -1,8 +1,12 @@
 from rest_framework.permissions import BasePermission
 
+from userApp.models import Role
+
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
+        if request.user.role is None:
+            return False
         if (request.user.role.name == 'Admin' or request.user.is_superuser) and request.user.is_authenticated:
             return True
         return False
