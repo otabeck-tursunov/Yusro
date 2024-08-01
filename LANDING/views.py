550e839670096a809101344d8e5bc1191822a3ab
Otabek Tusunov <otabecktursunov@gmail.com>
2024-08-01 22:24:37 +0500

    u: permission;

--- LANDING/views.py
+++ LANDING/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import *
 from rest_framework.views import APIView
@@ -11,8 +12,6 @@ from travelApp.models import *
 from userApp.serializers import *
 from userApp.models import *
 
-from userApp.permissions import IsAdminUser
-
 from articleApp.serializers import *
 from articleApp.models import Category
 
@@ -23,84 +22,84 @@ from companyApp.models import *
 # ------------- Company Information ----------------
 
 class MemberListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
 
 class MemberRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
 
 class ServiceListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 
 class ServiceRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 
 class ComfortListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Comfort.objects.all()
     serializer_class = ComfortSerializer
 
 
 class ComfortRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Comfort.objects.all()
     serializer_class = ComfortSerializer
 
 
 class PartnershipListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Partnership.objects.all()
     serializer_class = PartnershipSerializer
 
 
 class PartnershipRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Partnership.objects.all()
     serializer_class = PartnershipSerializer
 
 
 class TestimonialListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Testimonial.objects.all()
     serializer_class = TestimonialSerializer
 
 
 class TestimonialRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Testimonial.objects.all()
     serializer_class = TestimonialSerializer
 
 
 class QuestionListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
 
 class QuestionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
@@ -109,69 +108,69 @@ class QuestionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 # ------------- Article Posts ----------------
 
 class CategoryListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class ArticleListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
 class ArticleRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
 class CommentListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 
 class CommentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 
 class TagListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class TagRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class ArticleTagListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Article.objects.all()
     serializer_class = ArticleTagSerializer
 
 
 class ArticleTagRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Article.objects.all()
     serializer_class = ArticleTagSerializer
@@ -180,42 +179,42 @@ class ArticleTagRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 # ------------- Contact ----------------
 
 class DiscussionListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
 
 
 class DisscussionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
 
 
 class NewsletterListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
 
 
 class NewsletterRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
 
 
 class LidListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Lid.objects.all()
     serializer_class = LidSerializer
 
 
 class LidRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Lid.objects.all()
     serializer_class = LidSerializer
@@ -225,56 +224,56 @@ class LidRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 
 
 class TourTypeListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = TourType.objects.all()
     serializer_class = TourTypeSerializer
 
 
 class TourTypeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = TourType.objects.all()
     serializer_class = TourTypeSerializer
 
 
 class PlaceListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
 
 class PlaceRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
 
 class TourPackListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = TourPack.objects.all()
     serializer_class = TourPackSerializer
 
 
 class TourPackRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = TourPack.objects.all()
     serializer_class = TourPackSerializer
 
 
 class PackIncludeListCreateAPIView(ListCreateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = PackInclude.objects.all()
     serializer_class = PackIncludeSerializer
 
 
 class PackIncludeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = PackInclude.objects.all()
     serializer_class = PackIncludeSerializer
@@ -283,7 +282,7 @@ class PackIncludeRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 # --------------- User, Role ---------------------
 
 class UserRetrieveAPIView(RetrieveAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -293,7 +292,7 @@ class UserRetrieveAPIView(RetrieveAPIView):
 
 
 class UserUpdateAPIView(UpdateAPIView):
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsAuthenticated,)
 
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
